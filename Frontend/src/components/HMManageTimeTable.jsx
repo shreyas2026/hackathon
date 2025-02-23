daec96b510274b5faf7a412a320debaa5f92cd32
Somanath Mikali <somanathmikali@Somanaths-MacBook-Air.local>
2025-02-23 19:48:14 +0530

    two

--- Frontend/src/components/HMManageTimeTable.jsx
+++ Frontend/src/components/HMManageTimeTable.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { motion } from "framer-motion";
-import "./TimeTable.css";
 
 
 const baseurl = import.meta.env.VITE_BASE_URL;
